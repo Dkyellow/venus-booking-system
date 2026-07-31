9f75e96252f9117cd40dba8e0cf144e21e5c7464
Leslie Sarai <lesliesarai321@gmail.com>
2026-07-31 17:59:09 +0200

    comment

--- run.py
+++ run.py
@@ -87,6 +87,6 @@ with app.app_context():
         
         db.session.commit()
         print("[SEED] Done!")
-
+# new shit
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
